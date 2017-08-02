76b7a3f989b895d606a9124de4a98e7832558492
timofey1198 <timostar98@gmail.com>
2017-08-02 17:42:16 +0300

    изменил расстояние появления фона у панели навигации

--- resources/scripts/nav-panel_changer.js
+++ resources/scripts/nav-panel_changer.js
@@ -2,9 +2,9 @@
 window.onscroll = function() {
 	var dist = document.body.getBoundingClientRect().top;
 	var menu = document.getElementById("nav-panel");
-	if (dist < -200){
-		menu.style = "box-shadow: 0 5px 5px rgba(120, 120, 120, 0.5)";
-		menu.style.background = "#898989";
+	if (dist < -100){
+		menu.style = "box-shadow: 0 5px 5px rgba(50, 50, 50, 0.5)";
+		menu.style.background = "#323232";
 	}
 	else{
 		menu.style = "box-shadow: none";
